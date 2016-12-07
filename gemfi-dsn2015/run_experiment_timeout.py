42b40431c54ea3ab3ac34731920779b64f948a42
Zhiqiang Sui <zsui@umich.edu>
2016-12-07 16:16:49 -0500

    updated script

--- gemfi-dsn2015/run_experiment_timeout.py
+++ gemfi-dsn2015/run_experiment_timeout.py
@@ -9,10 +9,13 @@ def run(num_min, num_max):
 		cmd = "dmtcp_launch build/ALPHA/gem5.opt -re -d ./fi_output/GeneralFetchInjectedFault/qsort_small{} --debug-flags=FaultInjection configs/example/fs.py --cpu-type=detailed --caches --l2cache -b qsort_small1 --checkpoint-restore 1 --checkpoint-dir ./checkpoints/qsort_small --fi-in generated_faults/qsort_small/input_fault{} --restore-with-cpu detailed --switch-on-fault 0".format(str(i), str(i))
 		print(cmd)
 			
-		start = time.time()
-		subprocess.call(cmd, shell=True, timeout=120)
-		elapsed = (time.time() - start)
-		print("Elapsed time:", elapsed)	
+		try:	
+			start = time.time()
+			subprocess.call(cmd, shell=True, timeout=5)
+			elapsed = (time.time() - start)
+			print("Elapsed time:", elapsed)	
+		except:
+			print("Timeout Error. Continue Running")
 
 if __name__ == "__main__":
 	run(sys.argv[1], sys.argv[2])
